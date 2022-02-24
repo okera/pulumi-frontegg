424adf88334a99bcd976e2634601da1d51febbc0
Itay Neeman <itay@okera.com>
2022-02-24 14:21:14 -0800

    Build an initial version of the Frontegg package

--- provider/resources.go
+++ provider/resources.go
@@ -12,27 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package xyz
+package frontegg
 
 import (
 	"fmt"
 	"path/filepath"
 
+	frontegg "github.com/frontegg/terraform-provider-frontegg/provider"
+	"github.com/okera/pulumi-frontegg/provider/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/terraform-providers/terraform-provider-xyz/xyz"
 )
 
 // all of the token components used below.
 const (
 	// This variable controls the default name of the package in the package
 	// registries for nodejs and python:
-	mainPkg = "xyz"
+	mainPkg = "frontegg"
 	// modules:
-	mainMod = "index" // the xyz module
+	mainMod = "index" // the frontegg module
 )
 
 // preConfigureCallback is called before the providerConfigure function of the underlying provider.
@@ -46,41 +46,41 @@ func preConfigureCallback(vars resource.PropertyMap, c shim.ResourceConfig) erro
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
-	p := shimv2.NewProvider(xyz.Provider())
+	p := shimv2.NewProvider(frontegg.New(version.Version)())
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
-		P:           p,
-		Name:        "xyz",
+		P:    p,
+		Name: "frontegg",
 		// DisplayName is a way to be able to change the casing of the provider
 		// name when being displayed on the Pulumi registry
-		DisplayName: "",
+		DisplayName: "Frontegg",
 		// The default publisher for all packages is Pulumi.
 		// Change this to your personal name (or a company name) that you
 		// would like to be shown in the Pulumi Registry if this package is published
 		// there.
-		Publisher: "Pulumi",
+		Publisher: "Okera",
 		// LogoURL is optional but useful to help identify your package in the Pulumi Registry
 		// if this package is published there.
 		//
 		// You may host a logo on a domain you control or add an SVG logo for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL:     "",
+		LogoURL: "",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
 		PluginDownloadURL: "",
-		Description: "A Pulumi package for creating and managing xyz cloud resources.",
+		Description:       "A Pulumi package for creating and managing frontegg cloud resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
 		// https://www.pulumi.com/docs/guides/pulumi-packages/schema/#package.
-		Keywords:   []string{"pulumi", "xyz", "category/cloud"},
+		Keywords:   []string{"pulumi", "frontegg", "category/cloud"},
 		License:    "Apache-2.0",
-		Homepage:   "https://www.pulumi.com",
-		Repository: "https://github.com/pulumi/pulumi-xyz",
+		Homepage:   "https://www.frontegg.com",
+		Repository: "https://github.com/okera/pulumi-frontegg",
 		// The GitHub Org for the provider - defaults to `terraform-providers`
 		GitHubOrg: "",
-		Config:     map[string]*tfbridge.SchemaInfo{
+		Config:    map[string]*tfbridge.SchemaInfo{
 			// Add any required configuration here, or remove the example below if
 			// no additional points are required.
 			// "region": {
@@ -91,7 +91,7 @@ func Provider() tfbridge.ProviderInfo {
 			// },
 		},
 		PreConfigureCallback: preConfigureCallback,
-		Resources:            map[string]*tfbridge.ResourceInfo{
+		Resources: map[string]*tfbridge.ResourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi type. Two examples
 			// are below - the single line form is the common case. The multi-line form is
 			// needed only if you wish to override types or other default options.
@@ -104,11 +104,20 @@ func Provider() tfbridge.ProviderInfo {
 			// 		"tags": {Type: tfbridge.MakeType(mainPkg, "Tags")},
 			// 	},
 			// },
+			"frontegg_allowed_origin":      {Tok: tfbridge.MakeResource(mainPkg, mainMod, "AllowedOrigin")},
+			"frontegg_permission":          {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Permission")},
+			"frontegg_permission_category": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "PermissionCategory")},
+			"frontegg_redirect_uri":        {Tok: tfbridge.MakeResource(mainPkg, mainMod, "RedirectUri")},
+			"frontegg_role":                {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Role")},
+			"frontegg_tenant":              {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Tenant")},
+			"frontegg_webhook":             {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Webhook")},
+			"frontegg_workspace":           {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Workspace")},
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{
 			// Map each resource in the Terraform provider to a Pulumi function. An example
 			// is below.
 			// "aws_ami": {Tok: tfbridge.MakeDataSource(mainMod, "getAmi")},
+			"frontegg_permission": {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getPermission")},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			// List any npm dependencies and their versions
@@ -132,7 +141,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		Golang: &tfbridge.GolangInfo{
 			ImportBasePath: filepath.Join(
-				fmt.Sprintf("github.com/pulumi/pulumi-%[1]s/sdk/", mainPkg),
+				fmt.Sprintf("github.com/okera/pulumi-%[1]s/sdk/", mainPkg),
 				tfbridge.GetModuleMajorVersion(version.Version),
 				"go",
 				mainPkg,
